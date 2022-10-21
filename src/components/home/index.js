e96c977b0dde1c18199950e347293c4897f8c373
pradeep1412 <ppradeepkumar1412@gmail.com>
2022-10-21 15:52:36 +0530

    eslint

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -11,7 +11,7 @@ function MainSection() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         <div className='main_section_grid_item_2'>
-          <Image src={Fitz} className='home_section' />
+          <Image alt="" src={Fitz} className='home_section' />
         </div>
       </div>
       <div className='main_section_grid_container_2'>
@@ -19,7 +19,7 @@ function MainSection() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         <div className='main_section_grid_container_2_item_2'>
-          <Image src={Fits} className="main_section_grid_container_2"/>
+          <Image alt="" src={Fits} className="main_section_grid_container_2"/>
         </div>
       </div>
     </div>
